33d90be1f19e2775bdbec9c79b9185024e1dd78b
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-01-24 19:10:50 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Factorial{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter a number:");
+int n = sc.nextInt();
+int factorial = 1;
+for(int i=1;i<=n;i++)
+factorial*=i;
+{
+
+System.out.println("The Factorial of " + n + " is: " + factorial);
+}
+}
+}
