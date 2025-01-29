9e0ba256bc8ab59b48968db63450eec18bc10631
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-01-29 21:02:58 +0530

    Update SumOfDigits.java

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,17 +1,18 @@
+//calculates the sum of the digits of an input number.
 import java.util.Scanner;
 public class SumOfDigits{
 public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
 System.out.println("Enter a number:");
 int n = sc.nextInt();
-int Sum = 0;
+int sum = 0;
 while(n>0)
 {
-int R = n%10;
-Sum+=R;
+int digit = n%10;
+sum+=digit;
 n/=10;
 }
-System.out.println("The sum is:"+Sum);
+System.out.println("The sum is:"+sum);
 
 }
 }
